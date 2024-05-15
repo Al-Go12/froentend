6af16450fda81b7ef1010b73c9c57b44da9eb754
Al-Go12 <aravindkhari09@gmail.com>
2024-05-15 21:30:44 +0530

    url update

--- src/constant/api_url.js
+++ src/constant/api_url.js
@@ -1,1 +1,7 @@
-export  const BASE_URL = 'http://localhost:8000' ;
+
+
+
+
+const Protocol = window.location.protocol;
+const BaseDomain = 'socialnest.online:8000';
+const BASE_URL = `${Protocol}//${BaseDomain}`;
